3e56b37d18f4d79c450e9fc0c4a20636f49f4102
abot8639 <abot8639@gmail.com>
2026-01-23 23:39:10 +0200

    create buildSection

---
+++ lib/screens/orders_screen/widgets/build_section.dart
@@ -0,0 +1,16 @@
+  import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+Widget buildSection({required Widget child, EdgeInsets? padding}) {
+    return Container(
+      width: double.infinity,
+      padding: padding ?? const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Get.isDarkMode ? Colors.white.withOpacity(0.05) : Colors.grey.shade50,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Get.isDarkMode ? Colors.white10 : Colors.grey.shade200),
+      ),
+      child: child,
+    );
+  }
