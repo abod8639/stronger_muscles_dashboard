fefacf898fc457d94dfd36e8b0cfc108bce6455d
abot8639 <abot8639@gmail.com>
2026-01-23 23:50:55 +0200

    create buildAppBar

--- lib/screens/orders_screen/orders_screen.dart
+++ lib/screens/orders_screen/orders_screen.dart
@@ -70,7 +70,7 @@ class OrdersScreen extends StatelessWidget {
     );
   }
 }
-  PreferredSizeWidget buildAppBar(BuildContext context, OrdersController controller, ResponsiveLayout responsive) {
+  PreferredSizeWidget buildAppBar( OrdersController controller, ResponsiveLayout responsive) {
     return AppBar(
       title: Text(
         'إدارة الطلبات',
@@ -90,41 +90,5 @@ class OrdersScreen extends StatelessWidget {
         ),
         const SizedBox(width: 8),
       ],
-    );
-  }
-
-  Widget buildSearchBar( OrdersController controller) {
-    
-    return Builder(
-      builder: (context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
-        return Padding(
-          padding: context.responsive.defaultPadding,
-          child: Container(
-            decoration: BoxDecoration(
-              color: isDark ? Colors.white.withOpacity(0.05) : Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: isDark ? Colors.white10 : Colors.grey.shade300),
-            ),
-            child: TextField(
-              onChanged: controller.setSearchQuery,
-              decoration: InputDecoration(
-                hintText: 'البحث عن طلب برقم التعريف أو المبلغ...',
-                prefixIcon: const Icon(Icons.search, color: Colors.grey),
-                suffixIcon: Obx(() => controller.searchQuery.isNotEmpty 
-                  ? IconButton(
-                      icon: const Icon(Icons.clear, size: 20),
-                      onPressed: () {
-                        controller.setSearchQuery('');
-                      },
-                    ) 
-                  : const SizedBox.shrink()),
-                border: InputBorder.none,
-                contentPadding: const EdgeInsets.symmetric(vertical: 15),
-              ),
-            ),
-          ),
-        );
-      }
     );
   }
