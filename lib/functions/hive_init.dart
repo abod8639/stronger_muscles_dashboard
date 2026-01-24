3dbb37201e8dfd72b7a37a296ea53b79e13afe7f
abot8639 <abot8639@gmail.com>
2026-01-24 02:04:53 +0200

    create UserModelAdapter

--- lib/functions/hive_init.dart
+++ lib/functions/hive_init.dart
@@ -5,6 +5,7 @@ import 'package:stronger_muscles_dashboard/models/dashboard_user_model.dart';
 import 'package:stronger_muscles_dashboard/models/order_model.dart';
 import 'package:stronger_muscles_dashboard/models/product_model.dart';
 import 'package:stronger_muscles_dashboard/models/user_address.dart';
+import 'package:stronger_muscles_dashboard/models/user_model.dart';
 
 Future<void> hiveInit() async {
   await Hive.initFlutter();
@@ -14,11 +15,13 @@ Future<void> hiveInit() async {
   Hive.registerAdapter(CategoryModelAdapter());
   Hive.registerAdapter(DashboardUserAdapter());
   Hive.registerAdapter(UserAddressAdapter());
+  Hive.registerAdapter(UserModelAdapter());
 
   await Hive.openBox<DashboardUser>('dashboardUsers');
   await Hive.openBox<ProductModel>('products');
   await Hive.openBox<CategoryModel>('categories');
   await Hive.openBox<OrderModel>('orders');
   await Hive.openBox<UserAddress>('addresses');
+  await Hive.openBox<UserModel>('users');
 
 }
