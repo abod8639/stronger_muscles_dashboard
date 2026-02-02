e29b90343cdef5477920a600b68d8cb10dc0fc96
abot8639 <abot8639@gmail.com>
2026-02-03 00:42:02 +0200

    create buildMainIndicatorsSection

---
+++ lib/screens/dashboard_screen/widget/build_main_indicators_section.dart
@@ -0,0 +1,47 @@
+
+
+  import 'package:flutter/material.dart';
+import 'package:stronger_muscles_dashboard/config/responsive.dart';
+import 'package:stronger_muscles_dashboard/screens/dashboard_screen/widget/build_indicator_card.dart';
+import 'package:stronger_muscles_dashboard/screens/dashboard_screen/widget/build_section_title.dart';
+
+Widget buildMainIndicatorsSection(ResponsiveLayout res) {
+  final bool isSmallScreen = res.isMobile;
+
+  final int crossAxisCount = isSmallScreen
+      ? 1
+      : (res.screenWidth < 1200 ? 2 : 3);
+
+  final double childAspectRatio = isSmallScreen
+      ? 1.5
+      : (res.screenWidth < 1400 ? 1.1 : 1.5);
+
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      buildSectionTitle('المؤشرات الرئيسية', res),
+
+      SizedBox(height: res.itemSpacing),
+
+      GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: crossAxisCount,
+          childAspectRatio: childAspectRatio,
+          crossAxisSpacing: res.itemSpacing * 1.5,
+          mainAxisSpacing: res.itemSpacing * 1.5,
+        ),
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return AnimatedContainer(
+            duration: Duration(milliseconds: 300 + (index * 100)),
+            curve: Curves.easeOut,
+            child: buildIndicatorCard(index),
+          );
+        },
+      ),
+    ],
+  );
+}
+
