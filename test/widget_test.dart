7405d8cdf8119f59ed463ef67b9e7e2e209a28e2
abot8639 <abot8639@gmail.com>
2026-09-22 06:31:29 +0300

    test: configure GetX dependencies and mock services in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,54 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:stronger_muscles_dashboard/core/network/api_base.dart';
+import 'package:stronger_muscles_dashboard/core/network/api_service.dart';
+import 'package:stronger_muscles_dashboard/core/network/auth_service.dart';
+import 'package:stronger_muscles_dashboard/core/storage/cache_service.dart';
 import 'package:stronger_muscles_dashboard/main.dart';
 
-void main() {
-  testWidgets('Smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const StrongerMusclesDashboard());
+class MockAuthService extends AuthService {
+  @override
+  bool isLoggedIn() => false;
+  @override
+  String? getToken() => null;
+  @override
+  Map<String, String> getAuthHeaders() => {'Accept': 'application/json'};
+}
 
-    // Verify that platform version is retrieved.
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+      const MethodChannel('plugins.flutter.io/path_provider'),
+      (MethodCall methodCall) async {
+        return Directory.systemTemp.path;
+      },
+    );
+  });
+
+  setUp(() async {
+    Get.reset();
+    await GetStorage.init();
+    Get.put<AuthService>(MockAuthService(), permanent: true);
+    Get.put<ApiBase>(ApiBase(), permanent: true);
+    Get.put<ApiService>(ApiService(), permanent: true);
+    Get.put<CacheService>(CacheService(), permanent: true);
+  });
+
+  tearDown(() {
+    Get.reset();
+  });
+
+  testWidgets('Smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const StrongerMusclesDashboard());
+    await tester.pumpAndSettle();
+
     expect(
       find.byWidgetPredicate((Widget widget) => widget is Scaffold),
       findsWidgets,
