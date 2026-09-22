8d95facb204e9f1295f8612d9d88c34fa121a315
abot8639 <abot8639@gmail.com>
2026-09-22 06:24:46 +0300

    feat: add toEntity method to AddressModel and use it in OrderModel mapping

--- lib/features/orders/data/models/order_model.dart
+++ lib/features/orders/data/models/order_model.dart
@@ -4,6 +4,7 @@ import 'package:hive/hive.dart';
 import 'package:stronger_muscles_dashboard/data/models/address_model.dart';
 import 'package:stronger_muscles_dashboard/features/users/data/models/user_model.dart';
 import '../../domain/entities/order_entity.dart';
+import '../../domain/entities/address_entity.dart';
 
 part 'order_model.freezed.dart';
 part 'order_model.g.dart';
@@ -52,7 +53,7 @@ class OrderModel with _$OrderModel {
         paymentStatus: paymentStatus,
         paymentMethod: paymentMethod,
         addressId: addressId,
-        shippingAddress: shippingAddress,
+        shippingAddress: shippingAddress?.toEntity(),
         trackingNumber: trackingNumber,
         notes: notes,
         orderDate: orderDate,
