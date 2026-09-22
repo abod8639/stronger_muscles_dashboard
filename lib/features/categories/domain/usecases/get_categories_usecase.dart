807959f0793c8f17336b01d1bf4e2983470efca9
abot8639 <abot8639@gmail.com>
2026-09-22 06:18:18 +0300

    refactor: enable category domain logic, migrate to withValues for colors, and clean up dependencies

--- lib/features/categories/domain/usecases/get_categories_usecase.dart
+++ lib/features/categories/domain/usecases/get_categories_usecase.dart
@@ -1,14 +1,12 @@
-// import 'package:stronger_muscles_dashboard/features/categories/data/repositories/category_repository.dart';
+import '../entities/category_entity.dart';
+import '../repositories/category_repository.dart';
 
-// import '../entities/category_entity.dart';
-// import '../repositories/category_repository.dart';
+class GetCategoriesUseCase {
+  final CategoryRepository repository;
 
-// class GetCategoriesUseCase {
-//   final CategoryRepository repository;
+  GetCategoriesUseCase(this.repository);
 
-//   GetCategoriesUseCase(this.repository);
-
-//   // Future<List<CategoryEntity>> call({bool tree = true, bool forceRefresh = false}) async {
-//   //   return await repository.getCategories(tree: tree, forceRefresh: forceRefresh);
-//   // }
-// }
+  Future<List<CategoryEntity>> call({bool tree = true, bool forceRefresh = false}) async {
+    return await repository.getCategories(tree: tree, forceRefresh: forceRefresh);
+  }
+}
