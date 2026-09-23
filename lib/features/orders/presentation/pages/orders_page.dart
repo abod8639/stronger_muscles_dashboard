97a93157a29fa2024982694c90afa0c74f680852
abot8639 <abot8639@gmail.com>
2026-09-23 03:05:16 +0300

    refactor: update UI widgets across features to use Material 3 theme styling and standard cards

--- lib/features/orders/presentation/pages/orders_page.dart
+++ lib/features/orders/presentation/pages/orders_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:stronger_muscles_dashboard/config/app_colors.dart';
 import 'package:stronger_muscles_dashboard/config/responsive.dart';
 import 'package:stronger_muscles_dashboard/core/utils/components/base_app_bar.dart';
 import 'package:stronger_muscles_dashboard/core/utils/components/enhanced_loading_widget.dart';
