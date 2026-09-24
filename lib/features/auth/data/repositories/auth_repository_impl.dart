3b8eb12a9aaaa86bd022cd18517422fbb2e1175c
abot8639 <abot8639@gmail.com>
2026-09-24 11:44:38 +0300

    feat: add local user data persistence and retrieval in auth repository and service

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -3,6 +3,8 @@ import '../../domain/repositories/auth_repository.dart';
 import '../datasources/auth_local_datasource.dart';
 import '../datasources/auth_remote_datasource.dart';
 
+import '../models/auth_model.dart';
+
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
   final AuthLocalDataSource localDataSource;
@@ -18,6 +20,7 @@ class AuthRepositoryImpl implements AuthRepository {
     if (authModel.token != null) {
       await localDataSource.saveToken(authModel.token!);
     }
+    await localDataSource.saveUser(authModel.toJson());
     return authModel;
   }
 
@@ -27,6 +30,7 @@ class AuthRepositoryImpl implements AuthRepository {
     if (authModel.token != null) {
       await localDataSource.saveToken(authModel.token!);
     }
+    await localDataSource.saveUser(authModel.toJson());
     return authModel;
   }
 
@@ -43,5 +47,14 @@ class AuthRepositoryImpl implements AuthRepository {
   @override
   String? getToken() {
     return localDataSource.getToken();
+  }
+
+  @override
+  AuthEntity? getCurrentUser() {
+    final userMap = localDataSource.getUser();
+    if (userMap != null) {
+      return AuthModel.fromJson(userMap);
+    }
+    return null;
   }
 }
