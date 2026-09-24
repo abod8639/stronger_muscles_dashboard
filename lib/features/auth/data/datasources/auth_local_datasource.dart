3f54993298665c991481f9ad1b3ea9d97fe073d3
abot8639 <abot8639@gmail.com>
2026-09-24 11:44:17 +0300

    feat: add user data persistence and retrieval methods across auth layers

--- lib/features/auth/data/datasources/auth_local_datasource.dart
+++ lib/features/auth/data/datasources/auth_local_datasource.dart
@@ -5,11 +5,14 @@ abstract class AuthLocalDataSource {
   String? getToken();
   Future<void> clearToken();
   bool hasToken();
+  Future<void> saveUser(Map<String, dynamic> user);
+  Map<String, dynamic>? getUser();
 }
 
 class AuthLocalDataSourceImpl implements AuthLocalDataSource {
   final GetStorage storage;
   static const String _tokenKey = 'admin_token';
+  static const String _userKey = 'admin_user';
 
   AuthLocalDataSourceImpl(this.storage);
 
@@ -24,8 +27,23 @@ class AuthLocalDataSourceImpl implements AuthLocalDataSource {
   }
 
   @override
+  Future<void> saveUser(Map<String, dynamic> user) async {
+    await storage.write(_userKey, user);
+  }
+
+  @override
+  Map<String, dynamic>? getUser() {
+    final data = storage.read(_userKey);
+    if (data is Map) {
+      return Map<String, dynamic>.from(data);
+    }
+    return null;
+  }
+
+  @override
   Future<void> clearToken() async {
     await storage.remove(_tokenKey);
+    await storage.remove(_userKey);
   }
 
   @override
